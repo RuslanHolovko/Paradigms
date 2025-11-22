e630cd31f09b443fb8ba8302650904ef9600a94b
holovko.r <holovko.r@novaposhta.ua>
2025-11-22 14:48:44 +0200

    Implements Do

--- Tasks/Day1/2-do.js
+++ Tasks/Day1/2-do.js
@@ -1,6 +1,41 @@
 'use strict';
 
 // Put implementation here
+class DoSmth {
+  #value;
+  #fns = [];
+
+  constructor() {
+    return (value) => {
+      this.#fns = [];
+      this.#value = value;
+      return this;
+    }
+  }
+
+  bind(fn) {
+    this.#fns.push(fn);
+    return this;
+  }
+
+  run() {
+    return (...args) => {
+      let result = this.#value;
+
+      for (const fn of this.#fns) {
+        result = fn(result);
+      }
+
+      return (
+        typeof result === 'function'
+          ? result(...args)
+          : result
+      );
+    };
+  }
+}
+
+const Do = new DoSmth();
 
 Do({ id: 15 })
   .bind(({ id }) => ({ id, name: 'marcus', age: 42 }))
